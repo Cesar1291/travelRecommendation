ec9ab7477dd3b8d64c0d9174b966a76339fc1f9e
Cesar1291 <cesar_mist@outlook.com>
2026-01-29 12:16:28 -0600

    Agregando js

---
+++ travel_recommendation.js
@@ -0,0 +1,34 @@
+const btnSearch = document.getElementById('btnSearch');
+
+function searchDestiny() {
+        const input = document.getElementById('srcDestiny').value.toLowerCase();
+        const destinoDiv = document.getElementById('divDestino');
+        const displayDiv = document.getElementById('divDisplay');
+        destinoDiv.innerHTML = '';
+        displayDiv.innerHTML = '';
+
+        fetch('./travel_recommendation_api.json') 
+          .then(response => response.json())
+          .then(data => {
+            const countries = data.countries.find(item => item.name.toLowerCase() === input);
+
+            if (countries) { 
+              const Ciudades = countries.symptoms.join(', '); 
+              const description = countries.description;
+
+              destinoDiv.innerHTML += `<h2>${countries.name}</h2>`;
+              destinoDiv.innerHTML += `<img src="${countries.imagesrc}" alt="hjh">`;
+              displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${Ciudades}</p>`; 
+              displayDiv.innerHTML += `<p><strong>Descripción:</strong> ${description}</p>`;
+              
+            } else {
+              displayDiv.innerHTML = 'Destino no encontrado';
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            displayDiv.innerHTML = 'A ocurrido un error al recuperar el destino';
+          });
+ }
+
+      btnSearch.addEventListener('click', searchDestiny);
