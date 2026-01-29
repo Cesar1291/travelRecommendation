09fa51ebdc5839a06f5067c2aad53f521c14f1e7
Cesar1291 <cesar_mist@outlook.com>
2026-01-29 16:44:44 -0600

    Prueba js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -9,22 +9,20 @@ function searchDestiny() {
 
         fetch('./travel_recommendation_api.json') 
           .then(response => response.json())
-          //.then(response => console.log(response))
           .then(data => {
             const countries = data.countries.find(item => item.name.toLowerCase() === input);
 
-            //if (countries) { 
-            //  const Ciudades = countries.symptoms.join(', '); 
-            //  const description = countries.description;
-//
-            //  destinoDiv.innerHTML += `<h2>${countries.name}</h2>`;
-            //  destinoDiv.innerHTML += `<img src="${countries.imagesrc}" alt="hjh">`;
-            //  displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${Ciudades}</p>`; 
-            //  displayDiv.innerHTML += `<p><strong>Descripción:</strong> ${description}</p>`;
-            //  
-            //} else {
-            //  displayDiv.innerHTML = 'Destino no encontrado';
-            //}
+            if (countries) { 
+              const Ciudades = countries.symptoms.join(', '); 
+              const description = countries.description;
+              destinoDiv.innerHTML += `<h2>${countries.name}</h2>`;
+              destinoDiv.innerHTML += `<img src="${countries.imagesrc}" alt="hjh">`;
+              displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${Ciudades}</p>`; 
+              displayDiv.innerHTML += `<p><strong>Descripción:</strong> ${description}</p>`;
+              
+            } else {
+              displayDiv.innerHTML = 'Destino no encontrado';
+            }
           })
           .catch(error => {
             console.error('Error:', error);
