3ae439df0d042dea6d3f26790483caaa77b02896
Cesar1291 <cesar_mist@outlook.com>
2026-01-30 15:11:53 -0600

    correccion js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -12,47 +12,53 @@ function searchDestiny() {
     const input = document.getElementById('srcDestiny').value.toLowerCase();
     const destinoDiv = document.getElementById('divDestino');
     const displayDiv = document.getElementById('divDisplay');
-    const opcion = "";
-    //
+    var opcion = "";
+    var opcionEs = "";
+    var opcionId = 0;
+    
     destinoDiv.innerHTML = '';
     displayDiv.innerHTML = ''; 
     destinoDiv.style.opacity = 1;
     displayDiv.style.opacity = 1;
-    //
-    if (input=="pais"&&input=="paises"&&input=="countrie"&&input=="countries"){
+    
+    if (input=="pais"||input=="paises"||input=="countrie"||input=="countries"){
       opcion = "countries";
-    }else if(input=="templo"&&input=="templos"&&input=="temples"&&input=="temple"){
+      opcionEs = "Paises"
+      opcionId = 0;
+    }else if(input=="templo"||input=="templos"||input=="temples"||input=="temple"){
       opcion = "temples";
-    }else if(input=="playa"&&input=="playas"&&input=="beach"&&input=="beaches"){
+      opcionEs = "Templos"
+      opcionId = 1;
+    }else if(input=="playa"||input=="playas"||input=="beach"||input=="beaches"){
       opcion = "beaches";
+      opcionEs = "Playas"
+      opcionId = 2;
     }
+    
+    destinoDiv.innerHTML += `<h1 class="ttleMessage">${opcionEs}</h1>`;
 
     fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-            //
-            //const opciones = data.find(item => item.name.toLowerCase() === input);
-            const opciones = data.find(item => item.toLowerCase() === opcion);
-            if (opciones) {
-              console.log("opcion valida");
-              displayDiv.innerHTML = 'Destino no encontrado';
-            }else {
-              console.log("opcion valida");
-               displayDiv.innerHTML = 'opcion no valida';
-            }
-
-            //const countries = data.countries.find(item => item.name.toLowerCase() === input);
-            //if (countries) { 
-            //  const Ciudades = countries.cities;
-            //  destinoDiv.innerHTML += `<h1 class="ttleMessage">${countries.name}</h1>`;
-            //  for(var i=0; i<Ciudades.length; i++){
-            //    displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${Ciudades[i].name}</p>`; 
-            //    displayDiv.innerHTML += `<p><strong>Descripción:</strong>${Ciudades[i].description}</p>`;    
-            //    displayDiv.innerHTML += `<img src="${Ciudades[i].imageUrl}" width=50% height=auto alt="imagen-no-encontrada"><br><br>`; 
-            //  }   
-            //} else {
-            //  displayDiv.innerHTML = 'Destino no encontrado';
-            //}
+              if (opcionId===0){
+                const countries = data.countries;
+                for(var i=0;i<countries.length;i++){
+                    const Ciudades = countries[i].cities;
+                      //displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${countries.name}</p>`; 
+                      displayDiv.innerHTML += `<p><strong>Pais::</strong> ${countries[i].name}</p>`; 
+                    for(var j=0; j<Ciudades.length; j++){
+                      displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${Ciudades[j].name}</p>`; 
+                      displayDiv.innerHTML += `<p><strong>Descripción:</strong>${Ciudades[j].description}</p>`;    
+                      displayDiv.innerHTML += `<img src="${Ciudades[j].imageUrl}" width=50% height=auto alt="imagen-no-encontrada"><br><br>`; 
+                    }
+                }
+              }else if (opcionId===1){
+                  displayDiv.innerHTML += `<p>Trabajando en tus destinos</p>`;   
+              }else if (opcionId===2){
+                  displayDiv.innerHTML += `<p>Trabajando en tus destinos</p>`;   
+              } else {
+                displayDiv.innerHTML = 'Destino no encontrado';
+              }
     })
     .catch(error => {
       console.error('Error:', error);
