9d27ff610603f6af31dc38b53b4dcf701d00a007
Cesar1291 <cesar_mist@outlook.com>
2026-01-30 11:16:17 -0600

    Actualización de recursos

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -12,26 +12,47 @@ function searchDestiny() {
     const input = document.getElementById('srcDestiny').value.toLowerCase();
     const destinoDiv = document.getElementById('divDestino');
     const displayDiv = document.getElementById('divDisplay');
+    const opcion = "";
+    //
     destinoDiv.innerHTML = '';
     displayDiv.innerHTML = ''; 
     destinoDiv.style.opacity = 1;
     displayDiv.style.opacity = 1;
+    //
+    if (input=="pais"&&input=="paises"&&input=="countrie"&&input=="countries"){
+      opcion = "countries";
+    }else if(input=="templo"&&input=="templos"&&input=="temples"&&input=="temple"){
+      opcion = "temples";
+    }else if(input=="playa"&&input=="playas"&&input=="beach"&&input=="beaches"){
+      opcion = "beaches";
+    }
 
     fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-            const countries = data.countries.find(item => item.name.toLowerCase() === input);
-            if (countries) { 
-              const Ciudades = countries.cities;
-              destinoDiv.innerHTML += `<h1 class="ttleMessage">${countries.name}</h1>`;
-              for(var i=0; i<Ciudades.length; i++){
-                displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${Ciudades[i].name}</p>`; 
-                displayDiv.innerHTML += `<p><strong>Descripción:</strong>${Ciudades[i].description}</p>`;    
-                displayDiv.innerHTML += `<img src="${Ciudades[i].imageUrl}" width=50% height=auto alt="imagen-no-encontrada"><br><br>`; 
-              }   
-            } else {
+            //
+            //const opciones = data.find(item => item.name.toLowerCase() === input);
+            const opciones = data.find(item => item.toLowerCase() === opcion);
+            if (opciones) {
+              console.log("opcion valida");
               displayDiv.innerHTML = 'Destino no encontrado';
+            }else {
+              console.log("opcion valida");
+               displayDiv.innerHTML = 'opcion no valida';
             }
+
+            //const countries = data.countries.find(item => item.name.toLowerCase() === input);
+            //if (countries) { 
+            //  const Ciudades = countries.cities;
+            //  destinoDiv.innerHTML += `<h1 class="ttleMessage">${countries.name}</h1>`;
+            //  for(var i=0; i<Ciudades.length; i++){
+            //    displayDiv.innerHTML += `<p><strong>Ciudad::</strong> ${Ciudades[i].name}</p>`; 
+            //    displayDiv.innerHTML += `<p><strong>Descripción:</strong>${Ciudades[i].description}</p>`;    
+            //    displayDiv.innerHTML += `<img src="${Ciudades[i].imageUrl}" width=50% height=auto alt="imagen-no-encontrada"><br><br>`; 
+            //  }   
+            //} else {
+            //  displayDiv.innerHTML = 'Destino no encontrado';
+            //}
     })
     .catch(error => {
       console.error('Error:', error);
